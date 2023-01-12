416155667fd34e5c91d3c209e9ba7cdc86564ee5
Alex Arruda <alexrodrigoarruda@gmail.com>
2023-01-12 00:33:10 -0300

    Exibindo alternativas na tela

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const question = document.querySelector("#question");
 const answersBox = document.querySelector("#answers-box");
 const quizzContainer = document.querySelector("#quizz-container");
 const scoreContainer = document.querySelector("#score-container");
-const letter = ["a", "b", "c", "d"];
+const letters = ["a", "b", "c", "d"];
 let points = 0;
 let actualQuestion = 0;
 
@@ -94,6 +94,35 @@ function createQuestion(questionCurrent) {
 
   questionText.textContent = questions[questionCurrent].question;
   questionNumber.textContent = questionCurrent + 1;
+
+  // Inserir alternativas
+  questions[questionCurrent].answers.forEach((answer, index) => {
+    // Cria o template do botão do quizz
+    const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+
+    const letterBtn = answerTemplate.querySelector(".btn-letter");
+    const answerText= answerTemplate.querySelector(".question-answer");
+
+    letterBtn.textContent = letters[index];
+    answerText.textContent = answer["answer"];
+
+    answerTemplate.setAttribute("correct-answer", answer["correct"]);
+
+    // Remover as classes hide e answer-template
+    answerTemplate.classList.remove("hide");
+    answerTemplate.classList.remove("answer-template");
+
+    // Inserir as alternativas na tela
+    answersBox.appendChild(answerTemplate);
+
+    // Inserir evento no botão
+    answerTemplate.addEventListener("click", function() {
+
+    });
+  });
+
+  // Incrementar o número da questão
+  actualQuestion++;
 }
 
 // Inicialização do Quizz
